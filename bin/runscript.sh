e77dc0996da70e62eb86e28bd272730d34742cc0
dbiswa4 <db.tech07@gmail.com>
2015-12-13 23:47:15 -0800

    $$$$ Python Script and runscript added

---
+++ bin/runscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Killing the processes"
+kill $(ps aux | grep '[p]ython bin/Main_Hillary.py' | awk '{print $2}')
+kill $(ps aux | grep '[p]ython bin/Main_Trump.py' | awk '{print $2}')
+cd /home/ec2-user/BDAAProject
+echo "Starting Hillary Script"
+nohup python bin/Main_Hillary.py >> Log_Hillary.log &
+echo "Starting Trump Script"
+nohup python bin/Main_Trump.py >> Log_Trump.log &
